ea41a47cbc554221161041477d9fb64077aeb861
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2025-10-08 10:20:45 +0200

    listSecrets - versione 1

--- function_app.py
+++ function_app.py
@@ -1,30 +1,26 @@
 import azure.functions as func
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
 import os
-import datetime
-import json
 import logging
 
 app = func.FunctionApp()
 
-
 @app.route(route="welcome", auth_level=func.AuthLevel.ANONYMOUS)
-def sayHello(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+def listSecrets(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Function triggered to list Key Vault secrets.')
 
-    name = req.params.get('name')
-    if not name:
-        name = os.getenv("NAME", "Mondo") 
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    keyvault_name = "expriviaaikvf2t1yi" # os.getenv("KEYVAULT_NAME")
+    if not keyvault_name:
+        return func.HttpResponse("Missing KEYVAULT_NAME environment variable", status_code=500)
 
-    if name:
-        return func.HttpResponse(f"Hola, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized hola.",
-             status_code=200
-        )
+    vault_url = f"https://{keyvault_name}.vault.azure.net/"
+    credential = DefaultAzureCredential()
+    client = SecretClient(vault_url=vault_url, credential=credential)
+
+    try:
+        secrets = [s.name for s in client.list_properties_of_secrets()]
+        return func.HttpResponse("\n".join(secrets))
+    except Exception as e:
+        logging.error(f"Error accessing Key Vault: {e}")
+        return func.HttpResponse(f"Error: {str(e)}", status_code=500)
